e9d579015dc3d17cdb72f331c85e87d84d1fe0bc
yeiyei110 <yeisaac19829@gmail.com>
2023-03-16 04:59:01 -0600

    Nuevas clases

---
+++ 03-counter-app-vite/src/HelloWordApp.jsx
@@ -0,0 +1,14 @@
+ 
+// export function App(){
+//     // document.createElement
+//     return <h1>Hola mundo</h1>;
+// } 
+
+
+export const HelloWordApp = () => {
+  return (
+    <h1>HelloWor dApp</h1>
+  )
+}
+
+
